09d703f19e9c7ccd3d2f258023c4f902826878fb
thiago.henrique <theago.dev@gmail.com>
2025-05-18 12:02:24 -0300

    🏡 feat: creating index in order_id column.

---
+++ backend/migrations/2025_05_18_115920_create_index_order_table_order_id.php
@@ -0,0 +1,28 @@
+<?php
+
+use Hyperf\Database\Schema\Schema;
+use Hyperf\Database\Schema\Blueprint;
+use Hyperf\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('order', function (Blueprint $table) {
+            $table->index('order_id', 'idx_order_order_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('order', function (Blueprint $table) {
+            $table->dropIndex('idx_order_order_id');
+        });
+    }
+};
