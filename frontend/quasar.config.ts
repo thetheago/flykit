5d47b72b1e03d4b006d88fac0a8a12be70aa7fbf
thiago.henrique <theago.dev@gmail.com>
2025-05-20 08:45:23 -0300

    🎍 feat: doing check logic to login page + installing plugins.

--- frontend/quasar.config.ts
+++ frontend/quasar.config.ts
@@ -98,7 +98,10 @@ export default defineConfig((/* ctx */) => {
       // directives: [],
 
       // Quasar plugins
-      plugins: []
+      plugins: [
+        'Notify',
+        'Loading'
+      ]
     },
 
     // animations: 'all', // --- includes all animations
